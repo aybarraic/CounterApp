72a3965b310531917dfc4f5b823e5d0b310f1848
aybarraic <144291449+aybarraic@users.noreply.github.com>
2023-10-06 18:17:24 -0500

    Add files via upload

---
+++ CheckVoting.js
@@ -0,0 +1,8 @@
+function checkVotingEligibility(age){
+    //Conditional statement
+    if(age < 18){
+        console.log("Sorry,you are not eligible to vote yet!");
+    }else if (age >=18){
+        console.log("You are eligible to vote");
+    }
+}
